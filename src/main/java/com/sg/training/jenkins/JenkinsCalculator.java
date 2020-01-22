c5593758ea69a03fa72cedc7d230c633407c9b80
Shrijeeta <shrijeetaghosh@gmail.com>
2020-01-22 14:43:22 +0530

    Add files via upload

---
+++ src/main/java/com/sg/training/jenkins/JenkinsCalculator.java
@@ -0,0 +1,16 @@
+package com.sg.training.jenkins;
+
+public class JenkinsCalculator {
+	
+	public int addNumbers(int number1, int number2)
+	{
+		return number1+number2;
+	}
+	
+	public int subtractNumbers(int number1, int number2)
+	{
+		return number1-number2;
+		
+	}
+
+}
---
+++ src/test/java/com/sg/training/jenkins/JenkinsCaltest.java
@@ -0,0 +1,23 @@
+package com.sg.training.jenkins;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class JenkinsCaltest {
+	
+	@Test
+	public void addTest()
+	{
+		JenkinsCalculator myCalc = new JenkinsCalculator();
+		assertEquals(10,myCalc.addNumbers(5, 5));
+	}
+	
+	@Test
+	public void subtractTest()
+	{
+		JenkinsCalculator myCalc = new JenkinsCalculator();
+		assertEquals(5,myCalc.subtractNumbers(10, 5));
+	}
+
+}
